0fc0f2b3bbbea78e8516ea86aa786ec68747091d
ApexMaister <apexmaister@gmail.com>
2018-02-06 12:08:44 +0100

    Add files via upload

---
+++ ejercicio-edad-1.py
@@ -0,0 +1,7 @@
+#coding: utf-8 
+
+edad= input ("Introduce la edad:   ")
+
+if (edad >=15 and edad <=17):
+	
+	print "Puedes entrar a la sesion de tarde"
---
+++ ejercicio-edad-2.py
@@ -0,0 +1,7 @@
+#coding: utf-8 
+
+edad= input ("Introduce la edad:   ")
+
+if (edad >=18 and edad <=23):
+	
+	print "Puedes entrar a la sesion de jovenes"
