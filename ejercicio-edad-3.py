073fd0f67e135c5b9177b746150f09f62754c1c2
ApexMaister <apexmaister@gmail.com>
2018-02-08 12:09:43 +0100

    Add files via upload

---
+++ ejercicio-edad-3.py
@@ -0,0 +1,11 @@
+#coding: utf8
+
+edad= input("Introdueix la edat:   ")
+
+if (edad >=18 and edad <=23 or edad==17):
+
+    print "Puedes pasar a la sesion de jovenes "
+
+else:
+
+    print "No puedes pasar"
