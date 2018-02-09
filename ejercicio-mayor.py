b471d76b3f52a7378026f165770497b3bdec4fce
ApexMaister <apexmaister@gmail.com>
2018-02-09 12:59:52 +0100

    Add files via upload

---
+++ ejercicio-mayor.py
@@ -0,0 +1,26 @@
+#coding: utf-8
+
+A=input ("Introdueix el numero 1: ")
+B=input("Introdueix el numero 2:  ")
+C=input("Introdueix el numero 3:  ")
+
+if (A==B) and B==C)
+
+    print " Los tres son iguales "
+
+else:
+
+    if (A==B) and (C!=A) or
+        (A==C) and (B!=A) or
+         (B==C) and (A!=B):
+
+        print "Hay 2 iguales"
+
+
+
+    else:
+
+
+
+        if (A>B) and  (A<C)
+            pri
