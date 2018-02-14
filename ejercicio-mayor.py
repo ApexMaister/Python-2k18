2b18e8e74b2b4d0fb95d2c499f1e5326bf58986a
ApexMaister <apexmaister@gmail.com>
2018-02-14 23:12:04 +0100

    Add files via upload

---
+++ ejercicio-mayor.py
@@ -0,0 +1,19 @@
+#coding: utf8
+
+numero1= input("Introduce numero1:  ")
+numero2= input("Introduce numero2:  ")
+
+if (numero1 > numero2):
+
+    print numero1 , "Es mes gran que" , numero2
+
+
+else:
+
+    print numero2 , " Es mes gran que " numero1
+
+
+
+if (numero1 == numero2 ):
+
+    print "Son iguales"
