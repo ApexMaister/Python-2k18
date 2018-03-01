5ab5efa0aa8db0f9732665e23b944da5745f9014
ApexMaister <apexmaister@gmail.com>
2018-03-01 12:08:03 +0100

    Add files via upload

---
+++ conversor.py
@@ -0,0 +1,11 @@
+ #coding: utf-8
+
+print"""
+
+CONVERTIDOR DE CM A KM, M Y CM
+-------------------------------
+"""
+
+distanciaencm=input("Escriba una distancia en centimetros:   ")
+calculo= 
+print distanciaencm,"centi"
